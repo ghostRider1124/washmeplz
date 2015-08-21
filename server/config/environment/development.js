b08c0b625475fbc92067e5b3639403eea2fd533d
ghostRider <shauncfranks1124@gmail.com>
2015-08-21 13:49:12 -0400

    Friday second commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Development specific configuration
+/*module.exports = {
+ // MongoDB connection options
+ mongo: {
+ uri: 'mongodb://localhost/washmeplz-dev'
+ },
+
+ seedDB: true
+ };
+
+
+ */
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/washmeplz-dev'
+  },
+
+  seedDB: true
+
+};
+
+
